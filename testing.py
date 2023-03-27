10cef91cd83e60afba1eb2023ec89b2f19f0d4d9
Vilius-K <75629453+Vilius-K@users.noreply.github.com>
2023-03-27 12:48:48 +0300

    Naujas kompaileris

--- testing.py
+++ testing.py
@@ -1,33 +1,68 @@
 import subprocess
+import psutil
+import time
+import os
 
-Test_case_nr = int(input("Testu skaicius: "))
+Testu_sk = 1
 Tinkanciu_testu_sk = 0
+Laiko_limitas = 1 # SEKUNDES
+Atminties_limitas = 64 * 1024 * 1024 # BAITAI (64MB)
 
-# Compile the program
-subprocess.run(['g++', '-o', 'programa', 'programa.cpp'])
+# Sukompiliuojame programa
+try:
+    process = subprocess.run(["g++", "programa.cpp"], check = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+except subprocess.CalledProcessError as error:
+    print("Programa nesikompiliuoja")
 
-# Run each test case
-for test_case_id in range(1, Test_case_nr+1):
+# Tikriname programa su testais
+for Testo_id in range(1, Testu_sk+1):
 
-	# Read the test case input and output files
-    with open(f'{test_case_id}.in') as input_file:
-        test_input = input_file.read()
-    with open(f'{test_case_id}.sol') as output_file:
-        expected_output = output_file.read()
+	# Nuskaitome duomenu ir rezultatu failus
+    with open(f'{Testo_id}.in') as Duomenu_failas:
+        Duomenys = Duomenu_failas.read()
+    with open(f'{Testo_id}.sol') as Rezultatu_failas:
+        Rezultatai = Rezultatu_failas.read()
 
-    # Call the program with the test input
-    if test_case_id != 21:
-    	process = subprocess.run(['./programa'], input=test_input.encode(), capture_output=True)
-    	output = process.stdout
+    # Paleidziame sukompiliuota programa
+    process = subprocess.Popen(["./a.out"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate(input=Duomenys.encode())
 
-    # Use diff to compare the output with the expected results
-    result = subprocess.run(["diff", f'{test_case_id}.sol', "rezultatai.txt"], stdout=subprocess.PIPE)
-    if result.stdout:
-        print(test_case_id, " Netinka")
-        print(result.stdout.decode())
+    # Laukiame programos atsako
+    naudojamaAtmintis = psutil.Process(process.pid).memory_info().rss
+    pradziosLaikas = time.monotonic()
+    jauBuvoErroras = False
+
+    while True:
+            
+        # Tikriname, ar programa baige darba
+        returncode = process.poll()
+        if returncode is not None: break
+
+        # PATIKRINTI AR PROGRAMA VIRSIJO LAIKO LIMITA
+        praejasLaikas = time.monotonic() - pradziosLaikas
+        if praejasLaikas > Laiko_limitas:
+            process.kill()
+            print("Testas ", Testo_id, ": virsytas laiko limitas")
+            jauBuvoErroras = True  # Error'as buvo, nebespausdinti "Nera rezultatu failo"
+            break
+
+        # PATIKRINTI AR PROGRAMA VIRSIJO ATMINTIES LIMITA
+        sunaudotaAtmintis = psutil.Process(process.pid).memory_info().rss
+        if sunaudotaAtmintis > Atminties_limitas:
+            process.kill()
+            print("Testas ", Testo_id, ": virsytas atminties limitas")
+            jauBuvoErroras = True  # Error'as buvo, nebespausdinti "Nera rezultatu failo"
+            break
+
+    # Tikriname rezultatus
+    if not os.path.isfile("rezultatai.txt"):
+        print("Nėra rezultatų failo")
+        continue
+
+    rezultatai = subprocess.run(["diff", f'{Testo_id}.sol', "rezultatai.txt"], stdout=subprocess.PIPE)
+    
+    if rezultatai.stdout:
+        print("Testas", Testo_id, ": netinka rezultatu failas")
+        print(rezultatai.stdout.decode())
     else:
-        print(test_case_id, " Tinka")
-        Tinkanciu_testu_sk += 1
-
-    if test_case_id == Test_case_nr:
-        print(Tinkanciu_testu_sk, "/", Test_case_nr, "(", round((Tinkanciu_testu_sk*100)/Test_case_nr), "%)")
+        print("Testas", Testo_id, ": tinka")
