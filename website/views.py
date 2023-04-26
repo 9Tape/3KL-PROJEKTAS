6038fd3db5906f2e315cdf554c3eadbcaa262f70
Vilius Kazlauskas <vilius.kazlauskas@azuolynogimnazija.lt>
2023-04-26 23:39:08 +0300

    Bandymas

--- website/views.py
+++ website/views.py
@@ -9,6 +9,21 @@ import os
 
 views = Blueprint('views', __name__)
 
+def login_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+
+            if not current_user.is_authenticated():
+               return current_app.login_manager.unauthorized()
+            urole = current_app.login_manager.reload_user().get_urole()
+            if ( (urole != role) and (role != "ANY")):
+                return current_app.login_manager.unauthorized()      
+            return fn(*args, **kwargs)
+        return decorated_view
+    return wrapper
+
+
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
@@ -21,6 +36,11 @@ def home():
 def pdf(problem_id):
     path = f'/home/server/PDF_Salygos/{problem_id}.pdf'
     return send_file(path)
+
+@views.route('/admin')
+@login_required(role="SERVER-ADMIN")
+def admin(problem_id):
+    return "<h1>Admin Panel</h1>"
 
 
 @views.route('/problem/<problem_id>', methods=['GET', 'POST'])
